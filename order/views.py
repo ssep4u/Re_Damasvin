91a7ac1a6a045c9e391803c5d8d5f254ebd2f00a
ssep4u <ssep4u@gmail.com>
2020-07-07 23:34:36 +0900

    order/views total_price in OrderListView

--- order/views.py
+++ order/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView
@@ -12,6 +13,7 @@ class OrderListView(ListView):
   def get_context_data(self, *, object_list=None, **kwargs):
     context = super().get_context_data(**kwargs)
     context['menu_list'] = Drink.objects.all()
+    context['total_price'] = Order.objects.all().aggregate(total_price=Sum('price')).get('total_price', 0)
     return context
 
 
