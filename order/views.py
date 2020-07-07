708b0e91103080c7d325d78cef8966e61bdc9b4c
ssep4u <ssep4u@gmail.com>
2020-07-07 22:57:04 +0900

    order/views OrderCreateView, OrderUpdateView, order_delete

--- order/views.py
+++ order/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView
 
 from order.models import Order
 
 
 class OrderListView(ListView):
   model = Order
+
+
+class OrderCreateView(CreateView):
+  model = Order
+  fields = '__all__'
+  template_name_suffix = '_create'
+  success_url = reverse_lazy('order:list')
+
+
+class OrderUpdateView(UpdateView):
+  model = Order
+  fields = '__all__'
+  template_name_suffix = '_update'
+  success_url = reverse_lazy('order:list')
+
+
+def order_delete(request, pk):
+  order = Order.objects.get(id=pk)
+  order.delete()
+  return redirect('order:list')
